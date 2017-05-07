d9694462cc52d5d6608a89b77d8c8273831c22fe
ignis33 <julien.leotier@gmail.com>
2017-05-07 10:41:32 +0200

    test other computer

--- client/main.js
+++ client/main.js
@@ -1,22 +1,29 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+Cards = new Mongo.Collection('cards');
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.card.onCreated(function cardOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.card.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.card.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+Template.createCard.events({
+  'click button'(event, instance) {
+    Cards.insert(({}))
   },
 });
